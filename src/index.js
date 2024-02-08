1ef6a6c6002c190c3910574aa90a4c5a6f3f3505
brilord <95247252+Brilord@users.noreply.github.com>
2024-02-08 18:55:46 -0500

    committing

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const app = express();
 app.use(cors());
 const port = 3000;
 
+app.get('/', (req, res) => {
+    res.send('i am using docker')
+});
 app.get('/', (req, res) => {res.send('Hello World!')});
 
 app.get('/add/:n/:m', (req, res ) => {
@@ -17,32 +20,3 @@ app.get('/add/:n/:m', (req, res ) => {
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
-// const express = require("express");
-// const cors = require("cors");
-// const app = express();
-// const port = 3000;
-
-// app.use(express.static("public"));
-
-// app.use(cors);
-
-// app.get("/", (req, res) => {
-//   res.send("Arthmetic service - Hello World!");
-// });
-
-// app.get("/add", (req, res) => {
-//   const num1 = parseInt(req.query.num1, 10);
-//   const num2 = parseInt(req.query.num2, 10);
-
-//   if (!isNaN(num1) && !isNaN(num2)) {
-//     const sum = num1 + num2;
-
-//     res.json({ sum: sum });
-//   } else {
-//     res.status(400).json({ error: "Invalid numbers provided" });
-//   }
-// });
-
-// app.listen(port, () => {
-//   console.log("PORT RUNNNING!");
-// });
