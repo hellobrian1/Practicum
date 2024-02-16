d140292fd9c4b4bb01ac12a3bbb040fecff0bd75
brilord <briwon@iu.edu>
2024-02-15 19:35:20 -0500

    committing

--- src/index.js
+++ src/index.js
@@ -1,21 +1,48 @@
 const express = require('express');
-
 const cors = require('cors');
+
 const app = express();
 app.use(cors());
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('i am using docker')
-});
-app.get('/', (req, res) => {res.send('Hello World!')});
-
-app.get('/add/:n/:m', (req, res ) => {
-    console.log(req.params.n, req.params.m);
+// Addition route
+app.get('/add/:n/:m', (req, res) => {
+    console.log(req.params.n, "+", req.params.m);
     let sum = Number(req.params.n) + Number(req.params.m);
-    console.log(sum);
-    res.json(sum);
-})
+    console.log("Sum:", sum);
+    res.json({ result: sum });
+});
+
+// Subtraction route
+app.get('/subtract/:n/:m', (req, res) => {
+    console.log(req.params.n, "-", req.params.m);
+    let difference = Number(req.params.n) - Number(req.params.m);
+    console.log("Difference:", difference);
+    res.json({ result: difference });
+});
+
+// Multiplication route
+app.get('/multiply/:n/:m', (req, res) => {
+    console.log(req.params.n, "*", req.params.m);
+    let product = Number(req.params.n) * Number(req.params.m);
+    console.log("Product:", product);
+    res.json({ result: product });
+});
+
+// Division route
+app.get('/divide/:n/:m', (req, res) => {
+    console.log(req.params.n, "/", req.params.m);
+    let quotient = Number(req.params.n) / Number(req.params.m);
+    console.log("Quotient:", quotient);
+    res.json({ result: quotient });
+});
+
+// Default route
+app.get('/', (req, res) => {
+    console.log("Default route hit");
+    res.send('Hello World!');
+});
+
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
